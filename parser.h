26f200fa4061035f38956e9e14268d20b4c11664
rikki0405 <rikki-jones@hotmail.com>
2015-02-27 18:10:05 +0900

    Create parser.h

---
+++ parser.h
@@ -0,0 +1,26 @@
+//
+//  Parser.h
+//  Lua_TextAdv
+//
+//  Created by Rikki Jones on 2015/02/13.
+//  Copyright (c) 2015年 Rikki Jones. All rights reserved.
+//
+
+#ifndef __Lua_TextAdv__Parser__
+#define __Lua_TextAdv__Parser__
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <LuaBridge.h>
+
+extern "C" {
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+}
+
+// Will add stuff in here if needed
+
+#endif /* defined(__Lua_TextAdv__Parser__) */
