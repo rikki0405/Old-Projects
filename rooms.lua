8275b6ca289f5e207059465c270275a9776c87e5
rikki0405 <rikki-jones@hotmail.com>
2015-03-02 17:52:54 +0900

    Update rooms.lua
    Updated the lua file
    Removed Design1 as it was just a placeholder for testing purposes.
    Design2 renamed to Rooms, as this will be the current format for writing rooms

--- rooms.lua
+++ rooms.lua
@@ -1,37 +1,6 @@
 -- Rooms.lua
 
--- Main
---  Directions:
-
--- 0 - North; 1 - East; 2 - South; 3 - West; 4 - Up; 5 - Down
--- Max Directions: 6  |  No Exit: -1
-
---Design1 = {
-
---{ -- Room 1
---name = "Warehouse",
---exit[0] = -1,           -- North Exit
---exit[1] = -1,           -- East Exit
---exit[2] = -1,           -- South Exit
---exit[3] = "Docks",      -- West Exit
---exit[4] = -1,           -- Upper Exit
---exit[5] = -1,           -- Lower Exit
-
---},
-
---{ -- Room 2
---name = "Docks",
---exit[0] = "Gates",      -- North
---exit[1] = "Warehouse",  -- East
---exit[2] = -1,           -- South
---exit[3] = -1,           -- West
---exit[4] = -1,           -- Up
---exit[6] = -1            -- Down
---}
-
---}
-
-Design2 = {
+Rooms = {
 
     { -- Room 1
         name = "Warehouse",
@@ -42,10 +11,39 @@ Design2 = {
 
     { -- Room 2
         name = "Docks",
+        description = "The docks. This is one big shipping container yard.",
         east = "Warehouse",     -- to the east is the warehouse
         north = "Gates"        -- to the north is the gates
 
         -- conditions
        -- east_condition = ObjectRequired("Key")  -- Function Call implementation? Pointer?
+    },
+    
+    { -- Room 3
+        name = "Gates",
+        description = "The gates to the shipping yard is blocked off by a security gate.",
+        south = "Docks",
+        north = "Streets"
+    },
+    
+    { -- Room 4
+        name = "Streets",
+        description = "You glance down the street, yet see no activity in any direction",
+        south = "Docks",
+        west = "Cafe",
+        east = "Pier"
+    },
+    
+    { -- Room 5
+        name = "Cafe",
+        description = "A Cafe. At this time of night, it seems to be closed.",
+        east = "Streets"
+    },
+    
+    { -- Room 6
+        name = "Pier",
+        descripion = "A pier overlooking the ocean. You can see lights coming from within the Docks",
+        west = "Streets"
     }
+    
 }
