41d2d5646aadc6c2d50aa3babad6351f3f0d78d2
rikki0405 <rikki-jones@hotmail.com>
2015-03-24 21:04:29 +0900

    Create main.cpp

---
+++ Mar-2015/main.cpp
@@ -0,0 +1,47 @@
+//
+//  main.cpp - Main.cpp
+//  Lua_TextAdv_Engine
+//
+//  Created by Rikki Jones on 2015/01
+//  Copyright (c) 2015年 Rikki Jones. All rights reserved.
+//
+
+#include <LuaBridge.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+extern "C" {
+# include "lua.h"
+# include "lauxlib.h"
+# include "lualib.h"
+}
+
+/* New Classes for pointerDrivenPlayerTesting() */
+#include "nRooms.h"
+#include "nPlayer.h"
+#include "nParser.h"
+
+int zoneTesting() {
+    /* 24-Mar-2015: Testing Begins */
+    
+    /* nRooms zone testing */
+    
+    lua_State *L = luaL_newstate();
+    
+    /* Class Entities */
+    nRooms newRoom;
+    
+    /* Zone Load Test */
+    newRoom.loadZone(newRoom.current, L);
+    
+    return 0;
+}
+
+int main() {
+
+    zoneTesting();
+    
+    return 0;
+    
+}
